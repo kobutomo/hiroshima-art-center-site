f751ca01b416007373c3a91f9e499f2e8e7a6ab5
kobutomo <n.tomoro13@gmail.com>
2021-01-29 22:32:10 +0900

    i've done

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,53 +7,69 @@ import room from "../assets/aboutroompng.png"
 
 export default function About() {
   return (
-        <Layout>
-            <div>
-                <div>
-                    <SectionHeader text="ABOUT"/>
-                    <div style={{position:"relative", paddingTop: "22px", paddingBottom: "180px"}}>
-                        <img src = {room} alt = "Access" className = {style.room}></img>
-                        <div className={style.roomTextBox}>
-                            <p className={style.roomText}>広島芸術センターは広島在住の<br/>
-                                若手芸術家たちによって<br/>
-                                2010年から運営されています。<br/>
-                                ギャラリーとアトリエを備えており、<br/>
-                                広島の芸術家の発表と制作の場として<br/>
-                                活用されています。
-                                </p>
-                        </div>
-                    </div>
-                </div>
-                <div className = {style.mapContainer}>
-                    <SectionHeader text="ACCESS"/>
-                    <div>
-                        <img src = {map} alt = "Access" className = {style.map}></img>
-                        <div>
-                            <p className = {style.mapTextBold}>
-                            広島芸術センター</p>
-                            <p className = {style.mapText}>
-                            〒730-0825<br/>
-                            広島市中区光南2丁目17-1小中ビル1階<br/>
-                            <br/>
-                            広島バス24番吉島線<br/>
-                            「吉島病院入口」より徒歩5分<br/>
-                            ※駐車場はございません<br/>
-                            <br/>
-                            12:00-17:00　不定休
-                            </p>
-                        </div>
-                        
-                    </div>
-                </div>
-                <div>
-                    <SectionHeader text="CONTACT" id = "contact"/>
-                    <div>
-                        <p style = {{fontSize: "18px", textAlign: "center", letterSpacing: "7px"}}>
-                            hiroshima.geisen@gmail.com
-                        </p>
-                    </div>
-                </div>
+    <Layout>
+      <div>
+        <div>
+          <SectionHeader text="ABOUT" />
+          <div className={style.eyeCatch}>
+            <img src={room} alt="Access"></img>
+            <div className={style.roomTextBox}>
+              <p className={style.roomText}>
+                広島芸術センターは広島在住の
+                <br />
+                若手芸術家たちによって
+                <br />
+                2010年から運営されています。
+                <br />
+                ギャラリーとアトリエを備えており、
+                <br />
+                広島の芸術家の発表と制作の場として
+                <br />
+                活用されています。
+              </p>
             </div>
-        </Layout>
-    )
+          </div>
+        </div>
+        <section>
+          <SectionHeader text="ACCESS" />
+          <div className={style.mapContainer}>
+            <div className={style.map}>
+              <img src={map} alt="Access" />
+            </div>
+            <div className={style.mapTextContainer}>
+              <p className={style.mapTextBold}>広島芸術センター</p>
+              <p className={style.mapText}>
+                〒730-0825
+                <br />
+                広島市中区光南2丁目17-1小中ビル1階
+                <br />
+                <br />
+                広島バス24番吉島線
+                <br />
+                「吉島病院入口」より徒歩5分
+                <br />
+                ※駐車場はございません
+                <br />
+                <br />
+                12:00-17:00　不定休
+              </p>
+            </div>
+          </div>
+        </section>
+        <div>
+          <SectionHeader text="CONTACT" id="contact" />
+          <div>
+            <p
+              style={{
+                fontSize: "10px",
+                textAlign: "center",
+              }}
+            >
+              hiroshima.geisen@gmail.com
+            </p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
